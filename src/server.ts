ef86cf5cb40730a932f40eecd621febadf9ef1d1
alfredo-petri <alfredo_petri@hotmail.com>
2025-03-26 10:27:12 -0300

    feat: get courses

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from "express"
+import { knex } from "./database/knex"
 
 const app = express()
 app.use(express.json())
 
-app.get("/", async (request: Request, response: Response) => {
-  response.json({ message: "Hello World!" })
+app.get("/courses", async (request: Request, response: Response) => {
+    
+    const courses = await knex("courses").select().orderBy('name', 'asc')
+    // const courses = await knex.raw(`SELECT * FROM courses ORDER BY name ASC`)
+    
+    return response.json(courses.length ? courses : 'Não há cursos cadastrados' )
 })
 
 app.listen(3333, () => console.log(`Server is running on port 3333`))
