11fcb08947a497af55ae3f7f7142ff26c7a13c8d
alfredo-petri <alfredo_petri@hotmail.com>
2025-03-26 10:27:53 -0300

    feat: create course

--- src/server.ts
+++ src/server.ts
@@ -12,4 +12,13 @@ app.get("/courses", async (request: Request, response: Response) => {
     return response.json(courses.length ? courses : 'Não há cursos cadastrados' )
 })
 
+app.post("/courses", async (request: Request, response: Response) => {
+    const {name} = request.body
+    
+    await knex("courses").insert({ name })
+    // await knex.raw(`UPDATE courses SET name = ${name}  VALUES `)
+    
+    return response.status(201).json()
+})
+
 app.listen(3333, () => console.log(`Server is running on port 3333`))
